1af9504004c5f795f9ffa77f383c41be1b752f0d
Lbwk <72012473+Lbwk@users.noreply.github.com>
2021-01-31 16:39:24 +0100

    Add files via upload

---
+++ src/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed == 2;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixeUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        MovePlayer(horizontalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
